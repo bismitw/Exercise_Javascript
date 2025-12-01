1bf4f39dd0ed91843edcde3335d3c92cd24df941
Bismit Wagle <bismitwagle@gmail.com>
2025-12-01 12:30:50 +0530

    Add JavaScript Variable Function Exercise

---
+++ variablesExercise.js
@@ -0,0 +1,55 @@
+/*
+Perform the following mathematical operations
+on the provided variables a and b
+
+Add
+
+Subtract
+
+Multiply
+
+Divide
+
+Increment
+
+Decrement
+
+Reminder
+
+*/
+
+const a = 18;
+const b = 24;
+
+// Addition
+function add(a,b){
+
+    return a+b;
+}
+
+//Subtraction
+function subtract(a, b) {
+    return a - b;
+}
+//Multiplication
+function multiply(a, b) {
+    return a * b;
+}
+//Division
+function divide(a, b) {
+    return a / b;
+}
+//Increment
+function increment(a) {
+    return ++a ;
+}
+//Decrement
+function decrement(b){
+    return --b;
+}
+//Remainder
+function reminder(a, b) {
+    return a % b;
+}
+
+
